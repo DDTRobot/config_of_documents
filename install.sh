20825ffcdc95341aa6afe607ba0896d3a9c1a91c
Azhen <rongzhen939@gmail.com>
2025-07-16 16:17:30 +0800

    modify: install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ gcc -o wifi-app wifi-app.c
 cp wifi-app /usr/bin/
 systemctl restart systemd-networkd
 systemctl restart NetworkManager
-cp wifi-app.service /etc/systemd/system/
-systemctl daemon-reload
-systemctl enable wifi-app.service
-systemctl restart wifi-app.service
+# cp wifi-app.service /etc/systemd/system/
+# systemctl daemon-reload
+# systemctl enable wifi-app.service
+# systemctl restart wifi-app.service
